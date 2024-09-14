97786cc5737f8376f5ed33753e2157f6f94d4892
Kazi Wakil Ahmed <ahmedwakil66@gmail.com>
2024-09-15 02:25:11 +0600

    refresh button added

--- src/pages/dashboard/Page.jsx
+++ src/pages/dashboard/Page.jsx
@@ -72,9 +72,9 @@ const DashboardPage = () => {
     selectedIds.length > 0 && selectedIds.length < users.length;
 
   return (
-    <div>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-10">
-        <div className="p-2">
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-10">
+      <div className="p-2 flex justify-between">
+        <div>
           <button
             disabled={loading}
             onClick={() => onPressUpdate("update", "blocked")}
@@ -98,74 +98,72 @@ const DashboardPage = () => {
             {deleteIcon}
           </button>
         </div>
-        <table className="w-full text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                <button onClick={onPressSpecialSelect}>
-                  <Checkbox
-                    fill={allChecked ? "fill" : partialChecked ? "half" : ""}
-                  />
-                </button>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Email
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Last Login
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Status
-              </th>
-            </tr>
-          </thead>
-          <tbody className="relative">
-            {loading && (
-              <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-                <Spinner size="40" />
-              </div>
-            )}
-            {users.map((user) => (
-              <tr
-                key={user.id}
-                className="odd:bg-white even:bg-gray-50 border-b"
-              >
-                <th
-                  scope="row"
-                  className="px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  <div className="flex h-full items-center">
-                    <input
-                      onChange={onPressSelect}
-                      checked={!!selectedIds.find((id) => id == user.id)}
-                      type="checkbox"
-                      data-id={user.id}
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800"
-                    />
-                  </div>
-                </th>
-                <td className="px-6 py-4">{user.name}</td>
-                <td className="px-6 py-4">{user.email}</td>
-                <td className="px-6 py-4">
-                  {new Date(user.last_login_time).toLocaleString()}
-                </td>
-                <td
-                  className={`px-6 py-4 capitalize ${
-                    user.status === "blocked"
-                      ? "text-red-600"
-                      : "text-green-600"
-                  }`}
-                >
-                  {user.status}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <button onClick={getUsers} className="button">
+          Refresh
+        </button>
       </div>
+      <table className="w-full text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              <button onClick={onPressSpecialSelect}>
+                <Checkbox
+                  fill={allChecked ? "fill" : partialChecked ? "half" : ""}
+                />
+              </button>
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Email
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Last Login
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Status
+            </th>
+          </tr>
+        </thead>
+        <tbody className="relative">
+          {loading && (
+            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+              <Spinner size="40" />
+            </div>
+          )}
+          {users.map((user) => (
+            <tr key={user.id} className="odd:bg-white even:bg-gray-50 border-b">
+              <th
+                scope="row"
+                className="px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                <div className="flex h-full items-center">
+                  <input
+                    onChange={onPressSelect}
+                    checked={!!selectedIds.find((id) => id == user.id)}
+                    type="checkbox"
+                    data-id={user.id}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800"
+                  />
+                </div>
+              </th>
+              <td className="px-6 py-4">{user.name}</td>
+              <td className="px-6 py-4">{user.email}</td>
+              <td className="px-6 py-4">
+                {new Date(user.last_login_time).toLocaleString()}
+              </td>
+              <td
+                className={`px-6 py-4 capitalize ${
+                  user.status === "blocked" ? "text-red-600" : "text-green-600"
+                }`}
+              >
+                {user.status}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
